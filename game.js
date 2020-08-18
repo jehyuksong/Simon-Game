83963a850bdf90399cb477abb275f9a1c0f71538
SONG <SONG@SONGui-MacBook-Pro.local>
2020-08-18 17:52:44 +0900

    master
    master

---
+++ game.js
@@ -0,0 +1,74 @@
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let started = false;
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+  level++;
+  $("#level-title").text(`level ${level}`);
+  return gamePattern.push(randomChosenColor);
+}
+
+function playSound(name) {
+  let audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $(`#${currentColor}`).addClass("pressed");
+  setTimeout(function () {
+    $(`#${currentColor}`).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    const audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+    $("body").addClass("game-over");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("게임종료! 다시 시작하려면 아무 키나 일단 눌러~");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+
+$(document).keypress(function () {
+  if (started === false) {
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").on("click", function () {
+  let userChosenColor = this.id;
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  console.log(gamePattern);
+  console.log(userClickedPattern);
+  checkAnswer(userClickedPattern.length - 1);
+});
